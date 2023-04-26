beaa6dcde1c8cd6becc1503cdd3dd694c5e1a858
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-04-26 18:02:18 +0000

    js random ok

--- src/app.js
+++ src/app.js
@@ -9,4 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
-alert("Hola Ruben");
+
+let valores = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+let palo = ["corazon", "pica", " trebol", "diamante"];
+
+let valorRandom = valores[Math.floor(Math.random() * valores.length)];
+let paloRandom = palo[Math.floor(Math.random() * palo.length)];
+console.log(valorRandom, paloRandom);
